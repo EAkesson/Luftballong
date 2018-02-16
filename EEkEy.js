68777dcd59acf40eb998b2b530f6df11a6c45c13
Martin Marklund <martin.marklund@hotmail.se>
2018-02-16 11:20:14 +0100

    OrbitsControl now track the object. Implemented scroll listener allowing user to control camera position relative to tracked object.

--- EEkEy.js
+++ EEkEy.js
@@ -18,3 +18,43 @@ function onDocumentKeyUp(event) {
 	}
 
 };
+
+function onMouseWheel(event) {
+	var scrollDirection = detectMouseWheelDirection(event);
+	handleMouseWheelDirection(scrollDirection);
+}
+
+function handleMouseWheelDirection(direction){
+	if (direction == 'down') {
+		// To avoid wonky camera angles we limit the minimal value to 1.
+		if (relativeCamPos == 1) {
+			relativeCamPos = relativeCamPos;
+		}
+		else {
+			relativeCamPos --;
+		}
+	} else if (direction == 'up') {
+		relativeCamPos ++;
+	} else {
+		console.log("The scroll direction could not be detected");
+	}
+}
+
+function detectMouseWheelDirection(event) {
+	var delta = null, direction = false;
+
+	if (!event) { //If the event is not provided, we get it from the window object?
+		event = window.event;
+	}
+
+	if (event.wheelDelta) { // Will work in most cases
+		delta = event.wheelDelta / 60;
+		  } else if (evnet.detail) { // Fallback for Firefox
+			delta = -event.detail / 2;
+	}
+	if ( delta !== null ) {
+		direction = delta > 0 ? 'up' : 'down'
+	}
+
+	return direction;
+}
