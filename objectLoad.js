9241b249a51c442a7d6a16b2d21ddc58b89dceba
Tobias Olsson <tobbe_col@hotmail.com>
2018-02-22 16:36:28 +0100

    minor edit to the loading output

--- objectLoad.js
+++ objectLoad.js
@@ -3,7 +3,7 @@ var obj;
 var onProgress = function ( xhr ) {
 	if ( xhr.lengthComputable ) {
 		var percentComplete = xhr.loaded / xhr.total * 100;
-		console.log( Math.round(percentComplete, 2) + '% downloaded' );
+		console.log( Math.round(percentComplete) + '% downloaded' );
 	}
 };
 
