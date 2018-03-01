bc3a002271a313a64b1ca04648d846b349cf1bbb
Tobias Olsson <tobbe_col@hotmail.com>
2018-03-01 14:59:18 +0100

    spherical coordinates for envioroment

--- enviLoad.js
+++ enviLoad.js
@@ -1,3 +1,5 @@
+var fullTurn = 2*Math.PI;
+
 var onProgress = function ( xhr ) {
 	if ( xhr.lengthComputable ) {
 		var percentComplete = xhr.loaded / xhr.total * 100;
@@ -9,8 +11,14 @@ var onError = function ( xhr ) { };
 
 function enviLoad(enviObj, enviMtl){
 
-	var objLoader = new THREE.OBJLoader();
-	var mtlLoader = new THREE.MTLLoader();
+	var theta = Math.random() * (fullTurn - (-1*fullTurn)) + (-1*fullTurn); //random angle
+	var phi = Math.random() * (fullTurn - (-1*fullTurn)) + (-1*fullTurn); //random angle
 
+	obj.position.x = earthRadius*Math.sin(theta)*Math.cos(phi);
+	obj.position.y = earthRadius*Math.cos(theta);
+	obj.position.z = earthRadius*Math.sin(theta)*Math.sin(phi);
+
+	obj.rotation.x = phi;
+	obj.rotation.y = theta;
 
 }
