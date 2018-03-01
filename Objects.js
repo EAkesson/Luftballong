09639956ebbd6a5411af55788a53bce4429051c6
erikp <mail@eakesson.se>
2018-03-01 15:00:50 +0100

    Fixed an object object for the objects in the world

---
+++ Objects.js
@@ -0,0 +1,65 @@
+/**
+ * Created by erikp on 2018-03-01.
+ */
+
+var objec = {
+	Enviroment:
+	{
+		Trees : [
+			{
+				OBJpath: "/EnviModels/Tree1.obj",
+				MTLpath: "/EnviModels/Tree1.mtl"
+			},
+			{
+				OBJpath: "/EnviModels/Tree2.obj",
+				MTLpath: "/EnviModels/Tree2.mtl"
+			},
+			{
+				OBJpath: "/EnviModels/Tree3.obj",
+				MTLpath: "/EnviModels/Tree3.mtl"
+			},
+			{
+				OBJpath: "/EnviModels/Tree4.obj",
+				MTLpath: "/EnviModels/Tree4.mtl"
+			},
+			{
+				OBJpath: "/EnviModels/Tree5.obj",
+				MTLpath: "/EnviModels/Tree5.mtl"
+			}
+		],
+
+		Stones: [
+		{
+			OBJpath: "/EnviModels/Stone1.obj",
+			MTLpath: "/EnviModels/Stone1.mtl"
+		},
+		{
+			OBJpath: "/EnviModels/Stone2.obj",
+			MTLpath: "/EnviModels/Stone2.mtl"
+		},
+		{
+			OBJpath: "/EnviModels/Stone3.obj",
+			MTLpath: "/EnviModels/Stone3.mtl"
+		},
+		{
+			OBJpath: "/EnviModels/Stone4.obj",
+			MTLpath: "/EnviModels/Stone4.mtl"
+		},
+		{
+			OBJpath: "/EnviModels/Stone5.obj",
+			MTLpath: "/EnviModels/Stone5.mtl"
+		}
+		],
+
+		Other: [
+			{
+				OBJpath: "/EnviModels/Fences.obj",
+				MTLpath: "/EnviModels/Fences.mtl"
+			},
+			{
+				OBJpath: "/EnviModels/Bushes.obj",
+				MTLpath: "/EnviModels/Bushes.mtl"
+			}
+		]
+	}
+}
